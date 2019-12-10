4fa791ba3d89bc620f9846d3ec45aae9e2eaee16
Natan_Quezia <Natan_Quezia@DESKTOP-23VUJM5>
2019-12-10 00:20:27 -0300

    Teste Java

---
+++ tokio-test/bin/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO customer (id, name, email) VALUES
+(1, 'Natanael Gama', 'Natanael.n@hotmail.com'),
+(2, 'Jose Wilker -2', 'jose_wilker@hotmail.com'),
+(3, 'Mario Souza-3', 'mariosouza@hotmail.com'),
+(4, 'Fatima Caetano-4', 'fatimacaetano@hotmail.com'),
